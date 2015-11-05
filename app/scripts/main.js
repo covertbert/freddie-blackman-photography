5062e59cdb3b7cf07410bac80736466e63d8d6ea
Bertie Blackman <bertie.blackman@havaslynx.com>
2015-11-05 17:20:29 +0000

    Fixed mobile nav animation bug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,7 @@ var FBP = FBP || {};
 
 // Global Vairables
 var mobileNavOpen = $('.navigation--mobile__open-button'),
-		mobileNavClose = $('.navigation--mobile__close-button'),
+	mobileNavClose = $('.navigation--mobile__close-button'),
 	mobileNavOverlay = $('.navigation--mobile__overlay'),
 	mobileNavMenu = $('.navigation--mobile__overlay__menu');
 
@@ -16,14 +16,17 @@ FBP.mobileNav = {
 		"use strict";
 		mobileNavOpen.addClass('navigation--mobile__open-button--active');
 		mobileNavOverlay.removeClass('navigation--mobile__overlay--inactive').addClass('navigation--mobile__overlay--active');
-		mobileNavMenu.addClass('navigation--mobile__overlay__menu--active');
+		mobileNavMenu.removeClass('navigation--mobile__overlay__menu--inactive').addClass('navigation--mobile__overlay__menu--active');
 	},
 
 	close: function () {
 		"use strict";
 		mobileNavOpen.removeClass('navigation--mobile__open-button--active');
 		mobileNavOverlay.addClass('navigation--mobile__overlay--inactive').removeClass('navigation--mobile__overlay--active');
-		mobileNavMenu.addClass('navigation--mobile__overlay__menu--inactive');
+		mobileNavMenu.removeClass('navigation--mobile__overlay__menu--active').addClass('navigation--mobile__overlay__menu--inactive');
+		setTimeout(function(){
+			mobileNavMenu.removeClass('navigation--mobile__overlay__menu--inactive');
+		}, 400);
 	}
 
 };
